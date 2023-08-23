d666a6b28c98ba4f33ad11ccbd5300b04c5cdc14
Precious-Macaulay <preciousmacaulay001@gmail.com>
2023-08-23 22:01:38 +0100

    view customer added

--- src/app/dashboard/customers/page.jsx
+++ src/app/dashboard/customers/page.jsx
@@ -1,25 +1,58 @@
-'use client'
-import { IoIosArrowBack } from 'react-icons/io';
-import { IoIosArrowForward } from 'react-icons/io';
-import { Avatar, Input } from '@nextui-org/react';
-import { BiSearch } from 'react-icons/bi';
-import { FaFilter } from 'react-icons/fa';
+"use client";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { Avatar, Input } from "@nextui-org/react";
+import { BiSearch } from "react-icons/bi";
+import { FaFilter } from "react-icons/fa";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useAuthContext } from "@/src/context/AuthContext";
+import axios from "axios";
 
+export default function Customers() {
+  const { user } = useAuthContext();
+  const [userData, setUserData] = useState({});
+  const [selected, setSelected] = useState({});
 
-export default function Customers () {
-    return (<div className="h-screen w-screen p-special-m">
-        <div className='flex justify-between items-center m-special-x'>
-            <IoIosArrowBack />
-            <h1>Customers</h1>
-            <FaFilter />
+  // fetch user data from api/user
+  // and update user context
+
+  const fetchDoc = async () => {
+    const { data } = await axios.get(`/api/user/${user?.uid}`);
+    console.log(data);
+    setUserData(data);
+  };
+
+  useEffect(() => {
+    fetchDoc();
+  });
+
+  return (
+    <div className="h-screen w-screen p-special-m">
+      <div className="flex justify-between items-center m-special-x">
+        <IoIosArrowBack />
+        <h1>Customers</h1>
+        <FaFilter />
+      </div>
+      <Input
+        type="text"
+        variant="faded"
+        size="lg"
+        placeholder="search"
+        startContent={<BiSearch />}
+        color="#F5F5F5"
+        radius="sm"
+      />
+      {userData?.customers?.map((item) => (<div className="bg-white w-full m-special-xl p-special-tiny flex justify-between items-center">
+        <div className="flex flex-row items-center gap-special">
+          <Avatar />
+          <div>
+            <h1 className="small-text">{item.customerName}</h1>
+            <p className="light-text small-text">{item.email}</p>
+          </div>
         </div>
-        <Input type='text' variant='faded' size='lg' placeholder='search' startContent={<BiSearch />} color='#F5F5F5' radius='sm' />
-        <div className="bg-white w-full m-special-xl p-special-tiny flex justify-between items-center">
-            <div className='flex flex-row items-center gap-special'>
-            <Avatar name='B' />
-            <div><h1 className="">Bola Ahmed</h1><p className="light-text">bolaahmed@gmail.com</p></div>
-            </div>
-            <IoIosArrowForward />
-        </div>
-    </div>);
+        <IoIosArrowForward />
+      </div>))}
+    </div>
+  );
 }
