f71248b7e37fe122a02eb6afbed34a0c47991f2c
Precious-Macaulay <preciousmacaulay001@gmail.com>
2023-08-24 01:03:42 +0100

    remove push pay

--- src/app/api/pushpayment/route.js
+++ src/app/api/pushpayment/route.js
@@ -1,27 +1,27 @@
-import axios from "axios"
+// import axios from "axios"
 
-export default async function POST(request) {
-  const url = `/terminal//event`
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer sk_test_4fb07c0380a266230aedc1ff577781b198dd24a7`
-  }
+// export default async function POST(request) {
+//   const url = `/terminal//event`
+//   const headers = {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer sk_test_4fb07c0380a266230aedc1ff577781b198dd24a7`
+//   }
 
-  const {id, offline_reference} = JSON.parse(req.body)
+//   const {id, offline_reference} = JSON.parse(req.body)
 
-  const data = { 
-    type: "invoice",
-    action: "process",
-    data: {
-      id: id,
-      reference: offline_reference
-    }
-  }
+//   const data = { 
+//     type: "invoice",
+//     action: "process",
+//     data: {
+//       id: id,
+//       reference: offline_reference
+//     }
+//   }
 
-  try {
-    const response = await axios.post(url, data, { headers })
-    res.status(200).send(response.data)
-  } catch (error) {
-    res.status(500).send(error)
-  }
-}
+//   try {
+//     const response = await axios.post(url, data, { headers })
+//     res.status(200).send(response.data)
+//   } catch (error) {
+//     res.status(500).send(error)
+//   }
+// }
